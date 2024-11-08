d5b55e975dff78740a01a628db6613267f1dcd57
Admin <fortunamagix@gmail.com>
2024-11-08 22:29:50 +0700

    jsx 1

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,20 @@
-function ListGroup(){
-    return(
+function ListGroup() {
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo', 'London',
+        'Paris']
+
+    return (
         <>
-            <h1>List Group</h1>
+            <ul className="list-group">
+                {items.map(i =>
+                    <li>{i}</li>
+                )}
+
+            </ul>
         </>
     )
 }
+
 export default ListGroup
