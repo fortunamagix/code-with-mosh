533674ff27f6be2bb9b3be0d8f30a62fe8fc5a2b
Admin <fortunamagix@gmail.com>
2024-11-09 05:51:37 +0700

    jsx 2

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,23 @@
+// import message from "../Message.tsx";
+
 function ListGroup() {
-    const items = [
+    let items = [
         'New York',
         'San Francisco',
         'Tokyo', 'London',
         'Paris']
-
+    // items = []
+    // const message = items.length === 0 ? <p>No item found</p> : null
+    const getMessage = () => ( //better , can pass arg to this func
+        items.length === 0 ? <p>Item not found</p> : null
+    )
     return (
         <>
             <ul className="list-group">
-                {items.map(i =>
-                    <li>{i}</li>
-                )}
-
+                <h1>List</h1>
+                {/*message*/getMessage()}
+                {items.map((item, index) => <li className="list-group-item h5" key={index}
+                                                onClick={() => console.log('clicked')}>{item}</li>)}
             </ul>
         </>
     )
